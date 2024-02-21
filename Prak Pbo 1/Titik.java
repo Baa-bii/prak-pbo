71a3099cf33655610f7cfbd3d9b4c4d42285e813
bwa <117698535+Baa-bii@users.noreply.github.com>
2024-02-21 09:09:13 +0700

    Add files via upload

---
+++ Prak Pbo 1/Titik.java
@@ -0,0 +1,49 @@
+//Nama : Baron Albana Achmad
+//NIM : 24060122130073
+
+class Titik
+{
+	double absis, ordinat;
+	static double counterTitik;
+  
+//constructor
+	Titik()
+	{
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+	}
+
+	void setAbsis(double abs)
+	{
+		absis=abs;
+	}
+	
+	double getAbsis()
+	{
+		return absis;
+	}
+
+	void setOrdinat(double ord)
+	{
+		ordinat=ord;
+	}
+
+	double getOrdinat()
+	{
+		return ordinat;
+	}
+
+	double getCounterTitik()
+	{
+		return counterTitik;
+	}	
+
+	Titik(double abs, double ord)
+	{
+		absis = abs;
+		ordinat = ord;
+		counterTitik++;
+	}
+
+}

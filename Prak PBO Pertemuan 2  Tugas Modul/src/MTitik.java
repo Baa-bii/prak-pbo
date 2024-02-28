98631019913925e52f2916ebefdb8d5cf17048b3
bwa <117698535+Baa-bii@users.noreply.github.com>
2024-02-28 10:44:43 +0700

    Add files via upload

---
+++ Prak PBO Pertemuan 2  Tugas Modul/src/MTitik.java
@@ -0,0 +1,33 @@
+//class MTitik
+//{
+//    public static void main(String[] args)
+//    {
+//        Titik t1, t2, t3;
+//
+//        t1 = new Titik();
+//        t1.setAbsis(1);
+//        t1.setOrdinat(2);
+//
+//        t2 = new Titik();
+//        t2.setAbsis(3);
+//        t2.setOrdinat(4);
+//
+//        t3 = new Titik(5,6);
+//
+//
+//        System.out.println("Jumlah Titik = " + t1.getCounterTitik());
+//        System.out.println("t1(" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
+//        System.out.println("t2(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
+//        System.out.println("t3(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
+//        System.out.println("jarak t3=" + t3.getJarakPusat());
+//
+//        t1.refleksiX(t1);
+//        t2.refleksiY(t2);
+//
+//        System.out.println("t1 setelah refleksiX =(" + t1.getAbsis() + ", " + t1.getOrdinat() + ")");
+//        System.out.println("t2 setelah refleksiY =(" + t2.getAbsis() + ", " + t2.getOrdinat() + ")");
+//
+//        t3.getRefleksiX(t3);
+//        System.out.println("t3 setelah refleksiX =(" + t3.getAbsis() + ", " + t3.getOrdinat() + ")");
+//    }
+//}

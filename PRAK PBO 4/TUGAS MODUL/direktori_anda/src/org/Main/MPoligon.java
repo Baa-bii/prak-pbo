bcf2bfdd5777ced10d48526ec29def2bea1a99bd
bwa <117698535+Baa-bii@users.noreply.github.com>
2024-03-13 16:29:39 +0700

    Add files via upload

---
+++ PRAK PBO 4/TUGAS MODUL/direktori_anda/src/org/Main/MPoligon.java
@@ -0,0 +1,13 @@
+package org.Main;
+import org.bangundatar.*;
+
+public class MPoligon {
+    public static void main(String[] args) {
+        PersegiPanjang persegi = new PersegiPanjang(10, 10, 4);
+        Segitiga segitiga = new Segitiga(12, 10, 3);
+        persegi.printInfo();
+        segitiga.Info();    
+        System.out.println(("Luas Persegi Panjang adalah "+persegi.hitungLuas()));
+        System.out.println(("Luas Segitiga adalah "+segitiga.hitungLuas()));
+    }
+}
